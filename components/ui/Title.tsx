ce065c01ee54940287f8496b8cdf915f9fe1e85d
Angel Díaz <angeldiaz.hrn@gmail.com>
2026-03-05 14:50:21 -0600

    Componente Title con ícono

--- components/ui/Title.tsx
+++ components/ui/Title.tsx
@@ -1,8 +1,18 @@
+import { ComponentType } from "react";
+
 interface Props {
   text: string;
+  // Icon: ComponentType<React.SVGProps<SVGSVGElement>>;
   className?: string;
 }
 
 export const Title = ({ text, className }: Props) => {
-  return <div className={`mb-4 text-4xl font-bold ${className}`}>{text}</div>;
+  return (
+    <div
+      className={`mb-4 flex items-center gap-2 text-4xl font-bold ${className}`}
+    >
+      {/* {Icon && <Icon className="inline-block h-10 w-10" />} */}
+      {text}.
+    </div>
+  );
 };
